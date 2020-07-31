61e567de7776539a3a2f76d2357cc5f17896d7d0
peridot2029 <peridot2029@gmail.com>
2020-07-31 19:41:13 +0900

    redux store, action, dispatch, reducer

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import store from './store';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>Redux Basics</h1>
+    </div>
+  );
 }
 
 export default App;
