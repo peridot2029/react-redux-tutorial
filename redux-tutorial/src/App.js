b88c1644cee04cfdcd7d9cca392f446e82bfa28e
peridot2029 <peridot2029@gmail.com>
2020-08-05 19:45:29 +0900

    react router

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import store from './store';
-import './App.css';
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Redux Basics</h1>
-    </div>
+    <Router>
+      <header>
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+        <Link to='/about'>
+          <button>About</button>
+        </Link>
+        <Link to='/users'>
+          <button>Users</button>
+        </Link>
+      </header>
+      <hr />
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
